d206ed5c6d1ea8cf6a343116e0cb132d6a8662b3
santosh-chaugule <santoshchaugule1697@gmail.com>
2022-12-09 17:14:23 +0530

    C2

---
+++ script.js
@@ -0,0 +1,12 @@
+let news1 = {
+    //property:value
+
+    // Airfares Hike: The air travel costs have already been higher than the pre-pandemic levels all over the world.
+    w1: "Airfares",
+    w2: "Hike",
+    w3: `The`,
+    w4: 'air'
+}
+let word3 = "w3"
+//console.log
+console.log(news1.w1 + " " + news1.w2 + " " + news1[word3])
